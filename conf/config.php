f7ecb1e87e82cee063eec09dd9b16edd1187724c
gustavo <gustavo@laenredadera.net>
2017-01-16 21:05:09 +0100

    LDAP Configuration

--- conf/config.php
+++ conf/config.php
@@ -23,3 +23,38 @@ $config['db']['schemacheck'] = 'script';
 $config['site']['profile'] = 'gs_profile';
 
 addPlugin('Qvitter');
+
+addPlugin('ldapAuthentication', array(
+        'provider_name'=>'localhost',
+        'authoritative'=>true,
+        'autoregistration'=>true,
+        'email_changeable'=>false,
+        'password_changeable'=>false,
+        'password_encoding'=>'md5',
+        'host'=>array( 'localhost' ),
+        'starttls'=>false,
+        'basedn'=>'dc=yunohost,dc=org',
+        'attributes'=>array(
+                'username'=>'uid',
+                'nickname'=>'uid',
+                'email'=>'mail',
+                'fullname'=>'displayName',
+                'password'=>'unicodePwd')
+));
+
+addPlugin('ldapAuthorization', array(
+        'provider_name'=>'localhost',
+        'authoritative'=>true,
+        'login_group'=>'ou=users,dc=yunohost,dc=org',
+        'roles_to_groups'=>array(
+                // Create these roles migth be necessary
+                'moderator'=>'cn=moderators,ou=users,dc=yunohost,dc=org',
+                'administrator'=>'cn=admins,ou=users,dc=yunohost,dc=org'
+        ),
+        'basedn'=>'dc=yunohost,dc=org',
+        'host'=>array( 'localhost' ),
+        'starttls'=>false,
+        'attributes'=>array(
+                'username'=>'uid')
+));
+
